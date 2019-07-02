81f41a3ea63771aabe1fdd861c62de01846fe265
ecabrerar <eudris@gmail.com>
2019-07-02 16:05:47 -0400

    demo jconfdominicana

--- rest-assured-jconfdominicana/src/main/java/com/eudriscabrera/examples/java/JConfDominicanaEndPoint.java
+++ rest-assured-jconfdominicana/src/main/java/com/eudriscabrera/examples/java/JConfDominicanaEndPoint.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
@@ -35,30 +36,54 @@ public class JConfDominicanaEndPoint {
 				.collect(Collectors.toList())).build();
 	}
 
+	@GET
+	@Path("/sessions/{sessionId}")
+	@Produces("application/json")
+	public Response getSpeaker(@PathParam("sessionId") String sessionId) {
+		return Response.ok(new SessionResource().getSessions().stream().filter(session -> session.getId() == Integer.parseInt(sessionId))
+				.map(session->session)
+				.peek(session->System.out.println(session))
+				.collect(Collectors.toList())).build();
+	}
+
 	static class SessionResource {
 
 		private List<Session> getSessions() {
 
 			List<Session> sessions = new ArrayList<Session>();
-			sessions.add(new Session(101,"The Future of Java and You", "Heather VanCura", "8:30 AM"));
-			sessions.add(new Session(102,"¿Java EE? ¿Jakarta EE? ¿MicroProfile? ¿O tal vez, todos ellos?", "Sebastian Daschner",  "9:30 AM"));
-			sessions.add(new Session(103,"Bebiendo del Stream: Como usar las plataformas de mensajería para escalamiento y rendimiento", "Mark Heckler", "10:30 AM"));
-			sessions.add(new Session(104,"The Bootiful Application", "Josh Long", "2:30 PM"));
-			sessions.add(new Session(105,"RSocket - new Reactive cross-network Protocol", "Oleh Dokuka", "2:30 PM"));
-			sessions.add(new Session(106,"Unleashing Kotlin's power: A talk for Java developers", "Eury Perez", "2:30 PM"));
-			sessions.add(new Session(107,"Plataformas, herramientas y librerías políglotas en plataformas Java", "Eliezer Herrera", "2:30 PM"));
-			sessions.add(new Session(108,"Build, Test, Ship, Repeat: Effective testing and delivery using Continuous Integration", "Carlos Paulino", "03:30 PM"));
-			sessions.add(new Session(109,"Continuous code quality in java projects", "Igor Suhorukov", "03:30 PM"));
-			sessions.add(new Session(110,"Desarrollar Lenguajes DSL (Domain Specific Language) con Antlr", "Clivens Petit", "03:30 PM"));
-			sessions.add(new Session(111,"Serverless con Openfaas y Java", " Carlos Camacho", "03:30 PM"));
-			sessions.add(new Session(112,"Creando un webservice rapidamente sin multiples endpoints", "Omar Berroterán", "04:30 PM"));
-			sessions.add(new Session(113,"The Reactive Revolution", "Josh Long", "04:30 PM"));
-			sessions.add(new Session(114,"Dockerizing Java applications with Oracle Cloud", "Hilmer Chona", "04:30 PM"));
-			sessions.add(new Session(115," Pruebas de Integración para Servicios REST usando REST Assured", "Eudris Cabrera", "04:30 PM"));
-			sessions.add(new Session(116,"Java is back to the browser thanks to WebAssembly!", "Miguel Useche", "05:30 PM"));
-			sessions.add(new Session(117,"Cloud Native, Service-Meshed Java Enterprise With Istio", "Sebastian Daschner", "05:30 PM"));
-			sessions.add(new Session(118,"Emulate Amazon Web Services infrastructure in single JMV process to reduce", "Igor Suhorukov", "05:30 PM"));
-			sessions.add(new Session(119,"Paralelismo en java para todos", "Aluis Marte", "05:30 PM"));
+			sessions.add(new Session(101, "The Future of Java and You", "Heather VanCura", "8:30 AM"));
+			sessions.add(new Session(102, "¿Java EE? ¿Jakarta EE? ¿MicroProfile? ¿O tal vez, todos ellos?",
+					"Sebastian Daschner", "9:30 AM"));
+			sessions.add(new Session(103,
+					"Bebiendo del Stream: Como usar las plataformas de mensajería para escalamiento y rendimiento",
+					"Mark Heckler", "10:30 AM"));
+			sessions.add(new Session(104, "The Bootiful Application", "Josh Long", "2:30 PM"));
+			sessions.add(new Session(105, "RSocket - new Reactive cross-network Protocol", "Oleh Dokuka", "2:30 PM"));
+			sessions.add(
+					new Session(106, "Unleashing Kotlin's power: A talk for Java developers", "Eury Perez", "2:30 PM"));
+			sessions.add(new Session(107, "Plataformas, herramientas y librerías políglotas en plataformas Java",
+					"Eliezer Herrera", "2:30 PM"));
+			sessions.add(new Session(108,
+					"Build, Test, Ship, Repeat: Effective testing and delivery using Continuous Integration",
+					"Carlos Paulino", "03:30 PM"));
+			sessions.add(new Session(109, "Continuous code quality in java projects", "Igor Suhorukov", "03:30 PM"));
+			sessions.add(new Session(110, "Desarrollar Lenguajes DSL (Domain Specific Language) con Antlr",
+					"Clivens Petit", "03:30 PM"));
+			sessions.add(new Session(111, "Serverless con Openfaas y Java", " Carlos Camacho", "03:30 PM"));
+			sessions.add(new Session(112, "Creando un webservice rapidamente sin multiples endpoints",
+					"Omar Berroterán", "04:30 PM"));
+			sessions.add(new Session(113, "The Reactive Revolution", "Josh Long", "04:30 PM"));
+			sessions.add(
+					new Session(114, "Dockerizing Java applications with Oracle Cloud", "Hilmer Chona", "04:30 PM"));
+			sessions.add(new Session(115, "Pruebas de Integración para Servicios REST usando REST Assured",
+					"Eudris Cabrera", "04:30 PM"));
+			sessions.add(new Session(116, "Java is back to the browser thanks to WebAssembly!", "Miguel Useche",
+					"05:30 PM"));
+			sessions.add(new Session(117, "Cloud Native, Service-Meshed Java Enterprise With Istio",
+					"Sebastian Daschner", "05:30 PM"));
+			sessions.add(new Session(118, "Emulate Amazon Web Services infrastructure in single JMV process to reduce",
+					"Igor Suhorukov", "05:30 PM"));
+			sessions.add(new Session(119, "Paralelismo en java para todos", "Aluis Marte", "05:30 PM"));
 
 			return sessions;
 		}
--- rest-assured-jconfdominicana/src/test/java/com/eudriscabrera/examples/java/JConfDominicanaEndPointTest.java
+++ rest-assured-jconfdominicana/src/test/java/com/eudriscabrera/examples/java/JConfDominicanaEndPointTest.java
@@ -1,39 +1,48 @@
 package com.eudriscabrera.examples.java;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 
-import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import io.restassured.RestAssured;
-
-
+import io.restassured.http.ContentType;
 
 public class JConfDominicanaEndPointTest {
-	
-	@Before
-	public void setup() {
-	    RestAssured.baseURI = "http://localhost";
-	    RestAssured.port = 8080;
+
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 8080;
 	}
 
-	
 	@Test
 	public void should_response_status_code_200() {
-	 	
-			given().
-		    when().
-		        get("jconfdominicana/sessions")
-		        .then()
-		        .statusCode(200)
-		       ;
-		 
-		 
-		
-		    
-		       
-		
+
+		given().when().get("jconfdominicana/sessions").then().statusCode(200).and().contentType(ContentType.JSON);
+
+	}
+
+	@Test
+	public void should_return_status_code_200_and_session_with_id_101() {
+		String contenido = given().pathParam("sessionId", "101").when().get("jconfdominicana/sessions/{sessionId}")
+				.then()				
+				.statusCode(200).extract().asString();
+
+		Assert.assertTrue(!contenido.isEmpty());
+	}
+
+	@Test
+	public void should_return_status_code_200_and_session_with_title() {
+
+		given().pathParam("sessionId", "101").when().get("jconfdominicana/sessions/{sessionId}").then()
+				.contentType(ContentType.JSON)
+				.body("[0].id", equalTo(101))
+				.body("[0].title", equalTo("The Future of Java and You"));
+
 	}
 
 }
